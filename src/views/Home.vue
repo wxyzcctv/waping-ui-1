598deecde4114df1d98025da47cf56449b3180a5
王泽平 <wangzeping1@cloudwalk.com>
2021-04-14 20:29:48 +0800

    为Home页面添加css，并设置全局的中文字体

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <Topnav/>
+        <Topnav />
         <div class="banner">
             <h1>风UI</h1>
             <h2>一个简约的UI框架</h2>
@@ -14,11 +14,29 @@
 <script lang="ts">
 import Topnav from "../components/Topnav.vue"
 export default {
-    components:{
-        Topnav
-    }
-}
+    components:{ Topnav },
+};
 </script>
 <style lang="scss" scoped>
-
+.banner {
+  padding: 100px 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  background: lightgreen;
+  > .actions{
+    padding: 8px 0;
+    a {
+      margin:0 8px;
+      background: #fff;
+      display: inline-block;
+      $h: 28px;
+      height: $h;
+      line-height: $h;
+      border-radius: $h/2;
+      padding: 0 8px;
+    }
+  }
+}
 </style>
