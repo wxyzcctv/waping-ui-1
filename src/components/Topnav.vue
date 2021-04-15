292ad71dbc8aeb62cd897d328ccd450473518182
王泽平 <wangzeping1@cloudwalk.com>
2021-04-15 15:02:31 +0800

    完善整体布局

--- src/components/Topnav.vue
+++ src/components/Topnav.vue
@@ -27,7 +27,10 @@ export default {
     background: pink;
     display: flex;
     padding: 16px;
-    position: relative;
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
     z-index: 10;
     justify-content: center;
     align-items: center;
