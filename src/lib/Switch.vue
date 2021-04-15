18cf0266319761937c2dbbe50bcbc98a726d2209
王泽平 <wangzeping1@cloudwalk.com>
2021-04-15 16:38:16 +0800

    完成switch的样式并实现点击切换的效果

--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -1,5 +1,45 @@
 <template>
-    <div>
-        Switch 组件
-    </div>
+	<button :class="{ checked }" @click="toggle"><span></span></button>
 </template>
+<script lang="ts">
+import { ref } from "vue";
+export default {
+	setup() {
+		const checked = ref(false);
+		const toggle = () => {
+			checked.value = !checked.value;
+		};
+		return { checked, toggle };
+	},
+};
+</script>
+<style lang="scss">
+$h: 22px;
+$h2: $h - 4px;
+button {
+	height: $h;
+	width: $h * 2;
+	border: none;
+	background: grey;
+	border-radius: $h/2;
+	position: relative;
+}
+button:focus {
+	outline: none;
+}
+span {
+	position: absolute;
+	top: 2px;
+	left: 2px;
+	height: $h2;
+	width: $h2;
+	border-radius: $h2 / 2;
+	background: white;
+}
+button.checked {
+	background: blue;
+}
+button.checked > span {
+	left: calc(100% - #{$h2} - 2px);
+}
+</style>
