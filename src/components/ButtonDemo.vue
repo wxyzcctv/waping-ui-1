92558869739727f13720a6792ffbffe5fd7f079f
王泽平 <wangzeping1@cloudwalk.com>
2021-04-16 11:43:23 +0800

    button组件实现事件的传入

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -2,12 +2,20 @@
 	<div>Button示例</div>
 	<h1>示例1</h1>
 	<div>
-		<Button>你好</Button>
+		<Button @click="onClick" @focus="onClick" @mouseenter="onClick">
+			你好
+		</Button>
 	</div>
 </template>
 <script lang="ts">
 import Button from "../lib/Button.vue";
 export default {
 	components: { Button },
+	setup(props, context) {
+		const onClick = () => {
+			console.log("hi");
+		};
+		return { onClick };
+	},
 };
 </script>
--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -1,5 +1,17 @@
 <template>
-	<button>
-		<slot />
-	</button>
+	<div :size="size">
+		<button v-bind="rest">
+			<slot />
+		</button>
+	</div>
 </template>
+<script lang="ts">
+export default {
+	inheritAttrs: false,
+	props: {},
+	setup(props, context) {
+		const { size, ...rest } = context.attrs;
+		return { size, rest };
+	},
+};
+</script>
