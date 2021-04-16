099d8bf1173b4b6ac74ba7ef644b451c9293233d
王泽平 <wangzeping1@cloudwalk.com>
2021-04-16 15:54:19 +0800

    实例2中多增加了几个实例

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -13,6 +13,16 @@
 		<Button > 普通 </Button>
 		<Button size="small"> 小按钮 </Button>
 	</div>
+	<div>
+		<Button theme="link" size="big"> 大按钮 </Button>
+		<Button theme="link"> 普通 </Button>
+		<Button theme="link" size="small"> 小按钮 </Button>
+	</div>
+	<div>
+		<Button theme="text" size="big"> 大按钮 </Button>
+		<Button theme="text"> 普通 </Button>
+		<Button theme="text" size="small"> 小按钮 </Button>
+	</div>
 </template>
 <script lang="ts">
 import Button from "../lib/Button.vue";
