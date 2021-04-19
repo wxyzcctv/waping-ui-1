fa619aec825e66b98d6d8409a8622cf0d0dd1174
王泽平 <wangzeping1@cloudwalk.com>
2021-04-19 17:58:00 +0800

    增加tabs的样式

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,6 +1,23 @@
 <template>
-    <div v-for="(title,index) in titles" :key="index">{{title}}</div>
-    <component v-for="(con,index) in defaults" :is="con" :key="index"></component>
+	<div class="waping-tabs">
+		<div class="waping-tabs-nav">
+			<div
+				class="waping-tabs-nav-item"
+				v-for="(title, index) in titles"
+				:key="index"
+			>
+				{{ title }}
+			</div>
+		</div>
+		<div class="waping-tabs-content">
+			<component
+				class="waping-tabs-content-item"
+				v-for="(con, index) in defaults"
+				:is="con"
+				:key="index"
+			></component>
+		</div>
+	</div>
 </template>
 <script lang="ts">
 import Tab from "./Tab.vue";
@@ -12,13 +29,42 @@ export default {
 				throw new Error("Tabs子标签必须是Tab");
 			}
 		});
-        const titles = defaults.map(item => {
-            return item.props.title
-        })
+		const titles = defaults.map((item) => {
+			return item.props.title;
+		});
 		return {
 			defaults,
-            titles
+			titles,
 		};
 	},
 };
 </script>
+<style lang="scss">
+$blue: #40a9ff;
+$color: #333;
+$border-color: #d9d9d9;
+
+.waping-tabs{
+    &-nav{
+        display: flex;
+        color: $color;
+        border-bottom: 1px solid $border-color;
+
+        &-item{
+            padding: 8px 0;
+            margin: 0 16px;
+            cursor: pointer;
+
+            &:first-child{
+                margin-left: 0;
+            }
+            &.selected{
+                color: $blue;
+            }
+        }
+    }
+    &-content{
+        padding: 8px 0;
+    }
+}
+</style>
