6b046344eb48e9b46987ac90b366a6f873584af5
wxyzcctv <2846629226@qq.com>
2021-04-19 23:56:35 +0800

    使用watchEffect钩子函数优化代码

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -28,7 +28,7 @@
 	</div>
 </template>
 <script lang="ts">
-import { onMounted, onUpdated, ref } from "vue";
+import { onMounted, ref, watchEffect } from "vue";
 import Tab from "./Tab.vue";
 export default {
 	props: {
@@ -42,17 +42,20 @@ export default {
 		const indicator = ref<HTMLDivElement>(null);
 		const container = ref<HTMLDivElement>(null);
 
-		const x = () => {
-			const { width } = selectedItem.value.getBoundingClientRect();
-			indicator.value.style.width = width + "px";
-			const { left: left1 } = container.value.getBoundingClientRect();
-			const { left: left2 } = selectedItem.value.getBoundingClientRect();
-			const left = left2 - left1;
-			indicator.value.style.left = left + "px";
-		};
+		const x = () => {};
 
-		onMounted(x);
-		onUpdated(x);
+		onMounted(() => {
+			watchEffect(() => {
+				const { width } = selectedItem.value.getBoundingClientRect();
+				indicator.value.style.width = width + "px";
+				const { left: left1 } = container.value.getBoundingClientRect();
+				const {
+					left: left2,
+				} = selectedItem.value.getBoundingClientRect();
+				const left = left2 - left1;
+				indicator.value.style.left = left + "px";
+			});
+		});
 
 		const defaults = context.slots.default();
 		defaults.forEach((item) => {
