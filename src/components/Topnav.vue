dcca89480c68f728be57843b39b3244116b0c37b
wxyzcctv <2846629226@qq.com>
2021-04-26 08:49:23 +0800

    修改Home界面的主题颜色

--- src/components/Topnav.vue
+++ src/components/Topnav.vue
@@ -1,70 +1,69 @@
 <template>
-    <div class="topnav">
-        <div class="logo" >LOGO</div>
-        <ul class="menu">
-            <li>菜单1</li>
-            <li>菜单2</li>
-        </ul>
-        <svg class="icon toggleAside" aria-hidden="true" @click="toggleMenu">
-            <use xlink:href="#icon-caidan"></use>
-        </svg>
-    </div>
+	<div class="topnav">
+		<div class="logo">LOGO</div>
+		<ul class="menu">
+			<li>菜单1</li>
+			<li>菜单2</li>
+		</ul>
+		<svg class="icon toggleAside" aria-hidden="true" @click="toggleMenu">
+			<use xlink:href="#icon-caidan"></use>
+		</svg>
+	</div>
 </template>
 <script lang="ts">
-import {inject, Ref} from 'vue'
+import { inject, Ref } from "vue";
 export default {
-    setup() {
-        const asideVisible = inject<Ref<boolean>>('asideVisible')
-        const toggleMenu = () => {
-            asideVisible.value = !asideVisible.value
-        }
-        return { toggleMenu }
-    }
-}
+	setup() {
+		const asideVisible = inject<Ref<boolean>>("asideVisible");
+		const toggleMenu = () => {
+			asideVisible.value = !asideVisible.value;
+		};
+		return { toggleMenu };
+	},
+};
 </script>
 <style lang="scss" scoped>
 .topnav {
-    background: pink;
-    display: flex;
-    padding: 16px;
-    position: fixed;
-    top: 0;
-    left: 0;
-    width: 100%;
-    z-index: 10;
-    justify-content: center;
-    align-items: center;
-    > .logo {
-        max-width: 6em;
-        margin-right: auto;
-    }
-    > .menu {
-        display: flex;
-        white-space: nowrap;
-        flex-wrap: nowrap;
-        > li {
-            margin: 0 1em;
-        }
-    }
-    > .toggleAside {
-        width: 24px;
-        height: 24px;
-        position: absolute;
-        left: 16px;
-        top: 50%;
-        transform: translateY(-50%);
-        display: none;
-    }
-    @media (max-width: 500px){
-        > .menu{ 
-            display: none;
-        }
-        > .logo{
-            margin: 0 auto;
-        }
-        > .toggleAside {
-            display: inline-block;
-        }
-    }
+	display: flex;
+	padding: 16px;
+	position: fixed;
+	top: 0;
+	left: 0;
+	width: 100%;
+	z-index: 10;
+	justify-content: center;
+	align-items: center;
+	> .logo {
+		max-width: 6em;
+		margin-right: auto;
+	}
+	> .menu {
+		display: flex;
+		white-space: nowrap;
+		flex-wrap: nowrap;
+		> li {
+			margin: 0 1em;
+		}
+	}
+	> .toggleAside {
+		width: 24px;
+		height: 24px;
+		position: absolute;
+		left: 16px;
+		top: 50%;
+		transform: translateY(-50%);
+		display: none;
+	}
+	@media (max-width: 500px) {
+		> .menu {
+			display: none;
+		}
+		> .logo {
+			margin: 0 auto;
+		}
+		> .toggleAside {
+			display: inline-block;
+		}
+	}
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,42 +1,52 @@
 <template>
-    <div>
-        <Topnav />
-        <div class="banner">
-            <h1>风UI</h1>
-            <h2>一个简约的UI框架</h2>
-            <p class="actions">
-                <a href="https://github.com/wxyzcctv/waping-ui-1" target="_blank">Github</a>
-                <router-link to="doc">开始</router-link>
-            </p>
-        </div>
-    </div>
+	<div class="topnavAndBanner">
+		<Topnav />
+		<div class="banner">
+			<h1>风UI</h1>
+			<h2>一个简约的UI框架</h2>
+			<p class="actions">
+				<a
+					href="https://github.com/wxyzcctv/waping-ui-1"
+					target="_blank"
+					>Github</a
+				>
+				<router-link to="doc">开始</router-link>
+			</p>
+		</div>
+	</div>
 </template>
 <script lang="ts">
-import Topnav from "../components/Topnav.vue"
+import Topnav from "../components/Topnav.vue";
 export default {
-    components:{ Topnav },
+	components: { Topnav },
 };
 </script>
 <style lang="scss" scoped>
+.topnavAndBanner {
+	background: linear-gradient(
+		180deg,
+		rgba(255, 255, 255, 1) 0%,
+		rgba(247, 221, 217, 1) 100%
+	);
+}
 .banner {
-  padding: 100px 0;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  flex-direction: column;
-  background: lightgreen;
-  > .actions{
-    padding: 8px 0;
-    a {
-      margin:0 8px;
-      background: #fff;
-      display: inline-block;
-      $h: 28px;
-      height: $h;
-      line-height: $h;
-      border-radius: $h/2;
-      padding: 0 8px;
-    }
-  }
+	padding: 100px 0;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	flex-direction: column;
+	> .actions {
+		padding: 8px 0;
+		a {
+			margin: 0 8px;
+			background: #fff;
+			display: inline-block;
+			$h: 28px;
+			height: $h;
+			line-height: $h;
+			border-radius: $h/2;
+			padding: 0 8px;
+		}
+	}
 }
 </style>
