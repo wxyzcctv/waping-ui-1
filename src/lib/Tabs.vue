f9c754404faae5def068d826df384c7b226d9af8
wxyzcctv <2846629226@qq.com>
2021-10-11 08:39:09 +0800

    修改 watchEffect的bug

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -45,8 +45,23 @@ export default {
 			const left = left2 - left1;
 			indicator.value.style.left = left + "px";
 		};
-		onMounted(x);
-		onUpdated(x);
+		onMounted(() => {
+			watchEffect(
+				() => {
+					const { width } =
+						selectedItem.value.getBoundingClientRect();
+					indicator.value.style.width = width + "px";
+					const { left: left1 } =
+						container.value.getBoundingClientRect();
+					const { left: left2 } =
+						selectedItem.value.getBoundingClientRect();
+					const left = left2 - left1;
+					indicator.value.style.left = left + "px";
+				},
+				{ flush: "post" }
+			);
+		});
+		// onUpdated(x);
 
 		const defaults = context.slots.default();
 		defaults.forEach((item) => {
